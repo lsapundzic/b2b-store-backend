8085bece740e34e96debf10df8c375c34dd8da2a
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-05-07 19:50:12 +0200

    Removed content that I am not dealing with rn

--- src/api/distributorship/controllers/distributorship.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * distributorship controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::distributorship.distributorship');
